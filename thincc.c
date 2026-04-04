69fc7c2bcebdff3581daa1a5b1f4eb8898e7a93c
Matthew Draper <draper_matthewjohn@yahoo.com>
2026-04-04 09:53:44 -0700

    _read_file

--- thincc.c
+++ thincc.c
@@ -2,12 +2,32 @@
 #include "thincc.h"
 int main(void)
 {
-	u16 _H = 0x0048;
-	u16 _W = 0x0057;
-	u16 _lf = 0x000A;
-	mem[0x0000] = _H;
-	mem[0x0001] = _W;
-	out(mem[0x0000]);
-	out(mem[0x0001]);
-	out(_lf);
+	u16 _MEM_MOST = 0xFFFF;
+	u16 _EOF = 0xFFFF;
+	u16 _FILE_SIZE_MOST = 0x8000;
+
+	u16 _x = 0;
+	u16 _y = 0;
+	u16 _file_size = 0x0000;
+	
+_read_file:
+	_x = in();
+	if (_x == _EOF)
+		goto _success;
+
+	if (_file_size >= _FILE_SIZE_MOST)
+		goto _failure;
+
+	_y = _MEM_MOST;
+	_y -= _file_size;
+	mem[_y] = _x;
+	
+	_file_size += 0x0001;
+	goto _read_file;
+
+_success:
+	return 0;
+
+_failure:
+	return 1;
 }

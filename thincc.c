aaeaf4c790743461a4bfc387ae2714ffb0406e60
Matthew Draper <draper_matthewjohn@yahoo.com>
2026-04-05 14:41:22 -0700

    write_file

--- thincc.c
+++ thincc.c
@@ -5,7 +5,9 @@
 
 enum fn {
 	fn_read_file,
-	fn_write_file
+	
+	fn_write_file,
+	fn_write_file_loop,
 };
 
 DEF(d_0, 0x0000);
@@ -18,29 +20,62 @@ DEF(d_mem_most, 0xFFFF);
 DEF(d_eof, 0xFFFF);
 DEF(d_file_size_most, 0x8000);
 
-// size of file read in
-u16 g_file_size = d_0;
+// global argument registers
+u16 g_arg_0 = d_0;
+u16 g_arg_1 = d_0;
+u16 g_arg_2 = d_0;
 
 u16 step(u16 fn) {
 BRANCH(fn);
+
+	// fn_read_file
 	CASE(fn_read_file);
+		u16 v_file_size = g_arg_0;
 		u16 v_x = 0x0000;
 		u16 v_y = 0x0000;
 		
 		v_x = in();
-		if (v_x == d_eof)
-			return d_rc_exit;
+		if (v_x == d_eof) {
+			return fn_write_file;
+		}
 
-		if (g_file_size >= d_file_size_most)
+		if (v_file_size >= d_file_size_most) {
 			return d_rc_abort;
+		}
 
 		v_y = d_mem_most;
-		v_y -= g_file_size;
+		v_y -= v_file_size;
 		mem[v_y] = v_x;
 	
-		g_file_size += d_1;
+		v_file_size += d_1;
+		g_arg_0 = v_file_size;
 		return fn_read_file;
 	END;
+
+	// fn_write_file
+	CASE(fn_write_file);
+		g_arg_1 = d_0;
+		g_arg_2 = d_mem_most;
+		return fn_write_file_loop;
+	END;
+	CASE(fn_write_file_loop);
+		u16 v_file_size = g_arg_0;	
+		u16 v_i = g_arg_1;
+		u16 v_j = g_arg_2;
+
+		if (v_i == v_file_size){
+			return d_rc_exit;
+		}
+
+		out(mem[v_j]);
+		v_i += d_1;
+		v_j -= d_1;
+		g_arg_1 = v_i;
+		g_arg_2 = v_j;
+		return fn_write_file_loop;
+	END;
+
+	// unknown function?
 	DEFAULT;
 		return d_rc_abort;
 	END;

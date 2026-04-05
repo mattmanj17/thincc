6663f1244af407545677b9876b0c8ba4c68c2b1f
Matthew Draper <draper_matthewjohn@yahoo.com>
2026-04-05 10:46:40 -0700

    BRANCH CASE CUT
    make the step function:
    - a single switch statement
    - ... branching on fn

--- thincc.c
+++ thincc.c
@@ -1,7 +1,18 @@
 
+// thincc.c
+
 #include "thincc.h"
-u16 step(u16 fn)
-{
-	fn = fn;
-	return 0xFFFF;
+
+ENUM(_e_START, 0x0000);
+ENUM(_e_EXIT, 0xFFFF);
+
+u16 step(u16 fn) {
+BRANCH(fn);
+	CASE(_e_START);
+		return _e_EXIT;
+	END;
+	DEFAULT;
+		return _e_EXIT;
+	END;
+CUT;
 }
--- thincc.h
+++ thincc.h
@@ -1,15 +1,41 @@
+
 // thincc.h
 
+#if defined _thincc_h_
+#error thincc.h must not be included multiple times
+#else
+#define _thincc_h_
+#endif
+
+// includes
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <limits.h>
 
+// helper macros
+/*{*/
+#define STATEMENT(x) do { x } while (0)
+
+#define SPIN_EXIT 1
+#if SPIN_EXIT
+#define EXIT(x) STATEMENT((void)(x); while(1) {})
+#else
+#include <stdlib.h>
+#define EXIT(x) exit(x)
+#endif
+/*}*/
+
+// u16, the only type in thin c
 typedef uint16_t u16;
 
+// thin c "heap" memory is 64k 16 bit words.
+// distinct from globals/locals in the including file.
 u16 mem[UINT16_MAX+1];
 
-u16 in()
+// "syscalls"
+/*{*/
+// fgetc stdin
+u16 in(void)
 {
 	int ch = fgetc(stdin);
 	if (ch == EOF)
@@ -17,8 +43,7 @@ u16 in()
 		if (ferror(stdin))
 		{
 			perror("fgetc");
-		_halt_in:
-			goto _halt_in;
+			EXIT(1);
 		}
 		else
 		{
@@ -30,19 +55,21 @@ u16 in()
 		return (u16)ch;
 	}
 }
-
+// fputc stdout
 void out(u16 ch)
 {
-	if (fputc(ch, stdout) == EOF) 
+	if (fputc((unsigned char)ch, stdout) == EOF) 
 	{
 		perror("fputc");
-	_halt_out:
-		goto _halt_out;
+		EXIT(1);
 	}
 }
+/*}*/
 
+// main function. 
+// calls step function in a loop,
+// until it returns a sentinel.
 u16 step(u16 fn);
-
 int main(void)
 {
 	u16 fn = 0;
@@ -51,6 +78,20 @@ int main(void)
 		fn = step(fn);
 	} while (fn != 0xFFFF);
 
-_spin:
-	goto _spin;
+	EXIT(0);
 }
+
+// helper for the macros below
+#define REQ_SEMI STATEMENT(;)
+
+// macros intended to be consumed from thin c code
+/*{*/
+#define ENUM(name, value)	enum { name = value }
+
+#define BRANCH(fn)			switch (fn) { REQ_SEMI
+#define CUT					} REQ_SEMI
+
+#define CASE(fn)			case fn: { REQ_SEMI
+#define DEFAULT				default: { REQ_SEMI
+#define END					} break
+/*}*/

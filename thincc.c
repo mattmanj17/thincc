00fa859d299641c87e8250cf1795029ce3cd1bc1
Matthew Draper <draper_matthewjohn@yahoo.com>
2026-04-06 20:31:16 -0700

    ditch macros. format code.

--- thincc.c
+++ thincc.c
@@ -3,24 +3,23 @@
 
 #include "prelude.h"
 u16 mem[65536];
-enum { 
-	M_RC_ABORT	= 0xFFFE,
-	M_RC_EXIT	= 0xFFFF,
-	M_EOF		= 0xFFFF,
+enum {
+	M_RC_ABORT = 0xFFFE,
+	M_RC_EXIT = 0xFFFF,
+	M_EOF = 0xFFFF,
 };
 u16 step(u16 fn);
-int main(void)
-{
+int main(void) {
 	u16 v_fn = 0;
-	do
-	{
+	do {
 		v_fn = step(v_fn);
 	} while (v_fn != M_RC_EXIT && v_fn != M_RC_ABORT);
 
-	enum { DEBUG_SPIN = 1 };
+	enum {
+		DEBUG_SPIN = 1
+	};
 	volatile int v_debug_spin = DEBUG_SPIN;
-	if (v_debug_spin)
-	{
+	if (v_debug_spin) {
 	l_debug_spin:
 		goto l_debug_spin;
 	}
@@ -31,15 +30,6 @@ int main(void)
 	return (v_fn != M_RC_EXIT);
 }
 
-// macros intended to be consumed from thin c code
-/*{*/
-#define BRANCH(fn)			switch (fn) {
-#define CUT					}
-
-#define CASE(fn)			case fn: {
-#define DEFAULT				default: {
-#define END					} break
-/*}*/
 
 enum fn {
 	fn_start,
@@ -49,14 +39,14 @@ enum fn {
 };
 
 enum {
-	d_0= 0x0000,
-	d_1= 0x0001,
+	d_0 = 0x0000,
+	d_1 = 0x0001,
 
-	d_c_ch_file_size_max=		0x8000,
-	d_ptr_last=					0xFFFF,
+	d_c_ch_file_size_max = 0x8000,
+	d_ptr_last = 0xFFFF,
 
 	// d_ptr_last - d_c_ch_file_size_max + 1
-	d_ch_ptr_file_buffer_start= 0x8000
+	d_ch_ptr_file_buffer_start = 0x8000
 };
 
 // global argument registers
@@ -64,95 +54,105 @@ u16 g_arg_0 = d_0;
 u16 g_arg_1 = d_0;
 
 u16 step(u16 fn) {
-BRANCH(fn)
+	switch (fn) {
 
-	// void start();
-	CASE(fn_start);
-		// fill_file_buffer(0);
-		g_arg_0 = d_0;
-		return fn_fill_file_buffer;
-	END;
-
-	// void fill_file_buffer(u16 c_ch_file_size_cur);
-	CASE(fn_fill_file_buffer);
-		u16 v_c_ch_file_size_cur = g_arg_0;
-		u16 v_ch = d_0;
-		u16 v_ch_ptr = d_ch_ptr_file_buffer_start;
-		
-		// read
-		v_ch = in();
-		if (v_ch == M_EOF) {
-			// NOTE v_file_size is sitting in g_arg_0
-			return fn_print_file_buffer;
+		// void start();
+		case fn_start:
+		{
+			// fill_file_buffer(0);
+			g_arg_0 = d_0;
+			return fn_fill_file_buffer;
 		}
+		break;
 
-		// buffer exhasted
-		if (v_c_ch_file_size_cur >= d_c_ch_file_size_max) {
+		// void fill_file_buffer(u16 c_ch_file_size_cur);
+		case fn_fill_file_buffer:
+		{
+			u16 v_c_ch_file_size_cur = g_arg_0;
+			u16 v_ch = d_0;
+			u16 v_ch_ptr = d_ch_ptr_file_buffer_start;
+
+			// read
+			v_ch = in();
+			if (v_ch == M_EOF) {
+				// NOTE v_file_size is sitting in g_arg_0
+				return fn_print_file_buffer;
+			}
+
+			// buffer exhasted
+			if (v_c_ch_file_size_cur >= d_c_ch_file_size_max) {
+				return M_RC_ABORT;
+			}
+
+			// store
+			v_ch_ptr += v_c_ch_file_size_cur;
+			mem[v_ch_ptr] = v_ch;
+			v_c_ch_file_size_cur += d_1;
+
+			// recurse
+			g_arg_0 = v_c_ch_file_size_cur;
+			return fn_fill_file_buffer;
+		} 
+		break;
+
+		// void print_file_buffer(u16 c_ch_print);
+		case fn_print_file_buffer:
+		{
+
+			// exit if not supposed to print any ch's
+			if (g_arg_0 == d_0) {
+				return M_RC_EXIT;
+			}
+
+			// turn v_file_size into ch_ptr_last
+			// NOTE we must dec by 1 to get the "last" ptr
+			g_arg_0 -= d_1;
+			g_arg_0 += d_ch_ptr_file_buffer_start;
+
+			// move to ch_ptr_last
+			g_arg_1 = g_arg_0;
+
+			// ch_ptr_first = file_buffer_start
+			g_arg_0 = d_ch_ptr_file_buffer_start;
+
+			// call print_span
+			return fn_print_span;
+		} 
+		break;
+
+		// void print_span(u16 ch_ptr_first, u16 ch_ptr_last);
+		case fn_print_span:
+		{
+			u16 v_ch_ptr_first = g_arg_0;
+			u16 v_ch_ptr_last = g_arg_1;
+
+			if (v_ch_ptr_first > v_ch_ptr_last) {
+				// done printing
+				return M_RC_EXIT;
+			}
+
+			// print
+			out(mem[v_ch_ptr_first]);
+
+			// handle case where v_ch_ptr_last is the end of mem
+			if (v_ch_ptr_first == d_ptr_last) {
+				return M_RC_EXIT;
+			}
+
+			// inc
+			v_ch_ptr_first += d_1;
+
+			// recurse
+			g_arg_0 = v_ch_ptr_first;
+			return fn_print_span;
+		} 
+		break;
+
+		// unknown function?
+		default:
+		{
 			return M_RC_ABORT;
-		}
-
-		// store
-		v_ch_ptr += v_c_ch_file_size_cur;
-		mem[v_ch_ptr] = v_ch;
-		v_c_ch_file_size_cur += d_1;
-
-		// recurse
-		g_arg_0 = v_c_ch_file_size_cur;
-		return fn_fill_file_buffer;
-	END;
-
-	// void print_file_buffer(u16 c_ch_print);
-	CASE(fn_print_file_buffer);
-		
-		// exit if not supposed to print any ch's
-		if (g_arg_0 == d_0) {
-			return M_RC_EXIT;
-		}
-
-		// turn v_file_size into ch_ptr_last
-		// NOTE we must dec by 1 to get the "last" ptr
-		g_arg_0 -= d_1;
-		g_arg_0 += d_ch_ptr_file_buffer_start;
-		
-		// move to ch_ptr_last
-		g_arg_1 = g_arg_0;
-		
-		// ch_ptr_first = file_buffer_start
-		g_arg_0 = d_ch_ptr_file_buffer_start;
-		
-		// call print_span
-		return fn_print_span;
-	END;
-
-	// void print_span(u16 ch_ptr_first, u16 ch_ptr_last);
-	CASE(fn_print_span);
-		u16 v_ch_ptr_first = g_arg_0;	
-		u16 v_ch_ptr_last = g_arg_1;
-
-		if (v_ch_ptr_first > v_ch_ptr_last) {
-			// done printing
-			return M_RC_EXIT;
-		}
-
-		// print
-		out(mem[v_ch_ptr_first]);
-		
-		// handle case where v_ch_ptr_last is the end of mem
-		if (v_ch_ptr_first == d_ptr_last) {
-			return M_RC_EXIT;
-		}
-
-		// inc
-		v_ch_ptr_first += d_1;
-
-		// recurse
-		g_arg_0 = v_ch_ptr_first;
-		return fn_print_span;
-	END;
-
-	// unknown function?
-	DEFAULT
-		return M_RC_ABORT;
-	END;
-CUT
+		} 
+		break;
+	}
 }

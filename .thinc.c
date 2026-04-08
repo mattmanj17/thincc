7bd632394d1bd4ebc78ee124140432ee685ad3ae
Matthew Draper <draper_matthewjohn@yahoo.com>
2026-04-08 14:21:17 -0700

    ditch includes

--- .thinc.c
+++ .thinc.c
@@ -1,10 +1,24 @@
-
-#include <limits.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include ".thinc.h"
 
-u16  mem[USHRT_MAX + 1];
+#ifdef _MSC_VER
+    // XXX : harden this against different CRT versions
+    __declspec(dllimport) void* __cdecl __acrt_iob_func(unsigned _Ix);
+    #define M_STDIN\
+        (__acrt_iob_func(0))
+    #define M_STDOUT\
+        (__acrt_iob_func(1))
+    #define M_EOF\
+        (-1)
+#else
+    #error unsupported compiler
+#endif
+
+extern int fgetc(void* stream);
+extern int fputc(int ch, void* stream);
+extern int feof(void* stream);
+extern void perror(const char *s);
+
+u16  mem[U16_MAX + 1];
 u16  get(u16 ptr)          { return mem[ptr];          }
 u16  deref(u16 ptr_ptr)    { return mem[mem[ptr_ptr]]; }
 void set(u16 ptr, u16 val) { mem[ptr]  = val;          }
@@ -12,17 +26,17 @@ void sub(u16 ptr, u16 val) { mem[ptr] -= val;          }
 void add(u16 ptr, u16 val) { mem[ptr] += val;          }
 
 u16 in(void) {
-    int c = fgetc(stdin);
-    if (c != EOF)            
+    int c = fgetc(M_STDIN);
+    if (c != M_EOF)            
         return (u16)c;
-    if (feof(stdin))        
+    if (feof(M_STDIN))        
         return NIL;
     perror("fgetc");
     for (;;) {}
 }
 void out(u16 val) {
     u8 ch = (u8)val;
-    if (fputc(ch, stdout) != EOF) 
+    if (fputc(ch, M_STDOUT) != M_EOF) 
         return;
     perror("fputc");
     for (;;) {}

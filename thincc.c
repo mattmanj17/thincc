f2b1ecc5adb03b3b5f71aff42539efbfe5610a9f
Matthew Draper <draper_matthewjohn@yahoo.com>
2026-04-09 00:45:24 -0700

    GetStdHandle

--- thincc.c
+++ thincc.c
@@ -10,18 +10,57 @@ typedef unsigned short u16;
 M_VERIFY_NBIT_U(16);
 
 typedef unsigned int u32;
-#define U32_MAX 0xFFFFFFFFu
+#define U32_MAX 0xFFFFFFFFul
 M_VERIFY_NBIT_U(32);
 
 typedef unsigned long long u64;
-#define U64_MAX 0xFFFFFFFFFFFFFFFFu
+#define U64_MAX 0xFFFFFFFFFFFFFFFFull
 M_VERIFY_NBIT_U(64);
 
+#define M_K32FN(ret, fn) __declspec(dllimport) ret __stdcall fn
+#define M_INVALID_HANDLE ((void*)-1)
+
 void sys_exit(u32 exit_code) {
-    __declspec(dllimport) void __stdcall ExitProcess(u32 exit_code);
+    M_K32FN(void, ExitProcess(u32));
     ExitProcess(exit_code);
 }
 
+void* sys_stderr(void) {
+    M_K32FN(void*, GetStdHandle(u32));
+    void* h = GetStdHandle((u32)-12);
+    if (h == M_INVALID_HANDLE || h == 0){
+        // if we do not even have stderr,
+        //  we can not report errors,
+        //  so just die with a hopefully
+        //  noticable error code
+        sys_exit((u32)-12);
+    }
+    return h;
+}
+
+void* sys_stdout(void) {
+    M_K32FN(void*, GetStdHandle(u32));
+    void* h = GetStdHandle((u32)-11);
+    if (h == M_INVALID_HANDLE || h == 0){
+        // todo: diagnostic on stderr
+        sys_exit((u32)-11);
+    }
+    return h;
+}
+
+void* sys_stdin(void) {
+    M_K32FN(void*, GetStdHandle(u32));
+    void* h = GetStdHandle((u32)-10);
+    if (h == M_INVALID_HANDLE || h == 0){
+        // todo: diagnostic on stderr
+        sys_exit((u32)-10);
+    }
+    return h;
+}
+
 void entry(void) {
+    (void) sys_stdin();
+    (void) sys_stdout();
+    (void) sys_stderr();
     sys_exit(0);
 }
